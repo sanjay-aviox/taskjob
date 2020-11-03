ff2d92a045926ce3d5902bd3f95dc03dcc9749fd
Sanjay Dhiman <sanjay@avioxtechnologies.com>
2020-11-03 11:09:46 +0530

    balnk

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('contact') }}'><i class='nav-icon la la-question'></i> Contacts</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('region') }}'><i class='nav-icon la la-question'></i> Regions</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('property') }}'><i class='nav-icon la la-question'></i> Properties</a></li>
